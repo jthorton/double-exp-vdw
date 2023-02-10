629edd1643bbaf7a84fef952a5dde2b823334d1f
Pavan Behara <pavan.behara@openforcefield.org>
2023-02-10 08:37:18 -0800

    Replace json file with the correct one for reduced set (filtered records exlcuding P,S,F,I). Add scripts.

---
+++ inputs-and-results/benchmarks/qm-benchmarks/01-setup.py
@@ -0,0 +1,231 @@
+import logging
+import warnings
+from collections import defaultdict
+from multiprocessing import get_context
+from typing import Tuple
+
+import click
+import qcelemental
+from openeye import oechem
+from openff.qcsubmit.results import OptimizationResultCollection
+from openff.qcsubmit.results.filters import (
+    CMILESResultFilter,
+    ConnectivityFilter,
+    ElementFilter, RecordStatusFilter,
+    SMILESFilter,
+)
+from openff.toolkit.topology import Molecule
+from openff.toolkit.topology.molecule import SmilesParsingError
+from openff.toolkit.typing.engines.smirnoff import ForceField
+from openff.toolkit.utils import (
+    GLOBAL_TOOLKIT_REGISTRY,
+    OpenEyeToolkitWrapper,
+    UndefinedStereochemistryError,
+)
+from qcportal import FractalClient
+from qcportal.models.records import RecordStatusEnum
+from tqdm import tqdm
+
+N_PROCESSES = 8
+
+
+class InvalidCMILESFilter(CMILESResultFilter):
+    def _filter_function(self, result) -> bool:
+
+        try:
+            Molecule.from_mapped_smiles(result.cmiles, allow_undefined_stereo=True)
+        except (ValueError, SmilesParsingError):
+            return False
+
+        return True
+
+
+def _can_parameterize(smiles: str) -> Tuple[str, bool]:
+
+    try:
+
+        for toolkit in GLOBAL_TOOLKIT_REGISTRY.registered_toolkits:
+
+            if isinstance(toolkit, OpenEyeToolkitWrapper):
+                continue
+
+            GLOBAL_TOOLKIT_REGISTRY.deregister_toolkit(toolkit)
+
+        molecule = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
+        force_field = ForceField("openff-2.0.0.offxml")
+
+        force_field.create_openmm_system(molecule.to_topology())
+
+    except:
+        return smiles, False
+
+    return smiles, True
+
+
+def _process_molecule(record_and_molecule) -> oechem.OEMol:
+    """Convert a QC record and its associated molecule into an OE molecule which
+    has been tagged with the associated SMILES, final energy and record id."""
+
+    record, molecule = record_and_molecule
+
+    oe_molecule = molecule.to_openeye()
+    oechem.OE3DToInternalStereo(oe_molecule)
+
+    final_energy = record.get_final_energy() * qcelemental.constants.hartree2kcalmol
+
+    # add name and energy tag to the mol
+    oechem.OESetSDData(oe_molecule, "SMILES QCArchive", molecule.to_smiles())
+    oechem.OESetSDData(oe_molecule, "Energy QCArchive", str(final_energy))
+    oechem.OESetSDData(oe_molecule, "Record QCArchive", str(record.id))
+
+    return oe_molecule
+
+
+@click.command()
+@click.argument(
+    "data_set",
+    nargs=1,
+    type=click.STRING,
+    default="OpenFF Industry Benchmark Season 1 v1.1",
+)
+def main(data_set):
+    """Process and store optimized QC geometries from a QCArchive dataset.
+
+    The DATA_SET should likely be one of:
+
+    \b
+    * "OpenFF Full Optimization Benchmark 1"
+    * "OpenFF Industry Benchmark Season 1 v1.0"
+
+    The processed molecules tagged with information from the QC record, including the
+    CMILES and QC energy, will be stored in a new `01-processed-qm.sdf` file and
+    additionally information about the included records will be stored in
+    `industry-benchmark-opt-set-processed.json`.
+    """
+
+    warnings.filterwarnings("ignore")
+    logging.getLogger("openff.toolkit").setLevel(logging.ERROR)
+
+    # Make sure we consistently only use OE in this script
+    for toolkit in GLOBAL_TOOLKIT_REGISTRY.registered_toolkits:
+        if isinstance(toolkit, OpenEyeToolkitWrapper):
+            continue
+        GLOBAL_TOOLKIT_REGISTRY.deregister_toolkit(toolkit)
+
+    print("1a) Parsing collection")
+
+    client = FractalClient()
+    #
+    # result_collection = OptimizationResultCollection.from_server(
+    #     client=client,
+    #     datasets=data_set,
+    #     spec_name="default",
+    # )
+
+    result_collection = OptimizationResultCollection.parse_file(
+        'industry-benchmark-set-result-collection-downloaded.json')
+
+    ind_to_del = []
+    for i, item in enumerate(result_collection.entries['https://api.qcarchive.molssi.org:443/']):
+        if 'NH+' in item.cmiles or 'nH+' in item.cmiles:
+            ind_to_del.append(i)
+
+    for ind in sorted(ind_to_del, reverse=True):
+        print('deleting implicit hydrogen entry: ', ind)
+        del result_collection.entries['https://api.qcarchive.molssi.org:443/'][ind]
+
+    result_collection = result_collection.filter(
+        ElementFilter(
+            # The elements supported by SMIRNOFF
+            allowed_elements=["H", "C", "N", "O", "Cl", "Br"]
+        )
+    )
+
+    print("1b) Filtering unfinished / undesirable results")
+
+    result_collection = result_collection.filter(
+        InvalidCMILESFilter(),
+        RecordStatusFilter(status=RecordStatusEnum.complete),
+        ConnectivityFilter(),
+    )
+
+    _, molecules = zip(*result_collection.to_records())
+
+    unique_molecules = set()
+
+    for molecule in molecules:
+
+        # Re-perceive the stereochemistry from the final conformer.
+        oe_molecule = molecule.to_openeye()
+        oechem.OE3DToInternalStereo(oe_molecule)
+
+        try:
+            molecule = Molecule.from_openeye(oe_molecule)
+        except UndefinedStereochemistryError:
+            print(f"skipping {molecule.to_smiles()} - un-perceivable stereo")
+            continue
+
+        unique_molecules.add(molecule.to_smiles(isomeric=True, mapped=False))
+
+    with get_context("spawn").Pool(processes=N_PROCESSES) as pool:
+
+        filtered_smiles = {
+            smiles
+            for smiles, should_retain in tqdm(
+                pool.imap_unordered(_can_parameterize, unique_molecules),
+                total=len(unique_molecules),
+            )
+            if should_retain
+        }
+
+    result_collection = result_collection.filter(
+        SMILESFilter(smiles_to_include=[*filtered_smiles]),
+    )
+
+    print("1e) Grouping molecules")
+
+    records_and_molecules = result_collection.to_records()
+
+    grouped_molecules = defaultdict(list)
+
+    for record, molecule in records_and_molecules:
+
+        molecule = molecule.canonical_order_atoms()
+
+        smiles = molecule.to_smiles(isomeric=False, explicit_hydrogens=True)
+        grouped_molecules[smiles].append((record, molecule))
+
+    print("1f) Processing molecules")
+
+    processed_oe_molecules = [
+        _process_molecule(record_and_molecule)
+        for record_and_molecule in records_and_molecules
+    ]
+
+    print("1g) Writing processed molecules to sdf")
+
+    output_steam = oechem.oemolostream("01-processed-qm.sdf")
+
+    final_record_ids = set()
+
+    for i, oe_molecule in enumerate(processed_oe_molecules):
+
+        final_record_ids.add(oechem.OEGetSDData(oe_molecule, "Record QCArchive"))
+
+        oe_molecule.SetTitle(f"full_{i + 1}")
+        oechem.OEWriteConstMolecule(output_steam, oe_molecule)
+
+    output_steam.close()
+
+    result_collection.entries[client.address] = [
+        entry
+        for entry in result_collection.entries[client.address]
+        if entry.record_id in final_record_ids
+    ]
+
+    with open("01-processed-qm.json", "w") as file:
+        file.write(result_collection.json())
+
+
+if __name__ == "__main__":
+    main()
---
+++ inputs-and-results/benchmarks/qm-benchmarks/03-a-compute-metrics.py
@@ -0,0 +1,305 @@
+import json
+from typing import Any, Dict
+
+import click
+import numpy
+import numpy as np
+import openeye.oechem as oechem
+import pandas
+from geometric.internal import (
+    Angle,
+    Dihedral,
+    Distance,
+    OutOfPlane,
+    PrimitiveInternalCoordinates,
+)
+from geometric.molecule import Molecule as GeometricMolecule
+from openff.toolkit.topology import Molecule
+from rdkit.Chem import TorsionFingerprints
+from simtk import unit
+
+# Define the RMSD calculation parameters
+RMSD_AUTOMORPH = True  # take into acct symmetry related transformations
+RMSD_HEAVY_ONLY = False  # do consider hydrogen atoms for automorphisms
+RMSD_OVERLAY = True  # find the lowest possible RMSD
+
+def compute_rmsd(ref, tar, v_periodic=None):
+    """
+    Compute the RMSD between two arrays, supporting periodic difference
+    Code adapted from FORCEBALANCE
+    Source: https://github.com/leeping/forcebalance/blob/master/src/opt_geo_target.py
+    """
+    assert len(ref) == len(tar), 'array length must match'
+    n = len(ref)
+    if n == 0: return 0.0
+    if v_periodic is not None:
+        diff = periodic_diff(ref, tar, v_periodic)
+    else:
+        diff = ref - tar
+    rmsd = np.sqrt(np.sum(diff**2) / n)
+    return rmsd
+
+def periodic_diff(a, b, v_periodic):
+    ''' convenient function for computing the minimum difference in periodic coordinates
+    Code adapted from FORCEBALANCE
+    Source: https://github.com/leeping/forcebalance/blob/master/src/opt_geo_target.py
+
+    Parameters
+    ----------
+    a: np.ndarray or float
+        Reference values in a numpy array
+    b: np.ndarray or float
+        Target values in a numpy arrary
+    v_periodic: float > 0
+        Value of the periodic boundary
+    Returns
+    -------
+    diff: np.ndarray
+        The array of same shape containing the difference between a and b
+        All return values are in range [-v_periodic/2, v_periodic/2),
+        "( )" means exclusive, "[ ]" means inclusive
+    Examples
+    -------
+    periodic_diff(0.0, 2.1, 2.0) => -0.1
+    periodic_diff(0.0, 1.9, 2.0) => 0.1
+    periodic_diff(0.0, 1.0, 2.0) => -1.0
+    periodic_diff(1.0, 0.0, 2.0) => -1.0
+    periodic_diff(1.0, 0.1, 2.0) => -0.9
+    periodic_diff(1.0, 10.1, 2.0) => 0.9
+    periodic_diff(1.0, 9.9, 2.0) => -0.9
+    '''
+    assert v_periodic > 0
+    h = 0.5 * v_periodic
+    return (a - b + h) % v_periodic - h
+
+
+def _compute_internal_coordinate_rmsd(
+    molecule: Molecule,
+    qm_conformer: unit.Quantity,
+    mm_conformer: unit.Quantity,
+) -> Dict[str, float]:
+
+    qm_conformer = qm_conformer.value_in_unit(unit.angstrom)
+    mm_conformer = mm_conformer.value_in_unit(unit.angstrom)
+
+    geo_molecule = GeometricMolecule()
+    geo_molecule.Data = {
+        "resname": ["UNK"] * molecule.n_atoms,
+        "resid": [0] * molecule.n_atoms,
+        "elem": [atom.element.symbol for atom in molecule.atoms],
+        "bonds": [(bond.atom1_index, bond.atom2_index) for bond in molecule.bonds],
+        "name": molecule.name,
+        "xyzs": [qm_conformer],
+    }
+
+    internal_coordinate_generator = PrimitiveInternalCoordinates(geo_molecule)
+
+    internal_coordinate_types = {
+        "Bond": Distance,
+        "Angle": Angle,
+        "Dihedral": Dihedral,
+        "Improper": OutOfPlane,
+    }
+
+    internal_coordinates = {
+        label: [
+            (
+                internal_coordinate.value(qm_conformer),
+                internal_coordinate.value(mm_conformer),
+            )
+            for internal_coordinate in internal_coordinate_generator.Internals
+            if isinstance(internal_coordinate, internal_coordinate_class)
+        ]
+        for label, internal_coordinate_class in internal_coordinate_types.items()
+    }
+
+    internal_coordinate_rmsd = {}
+
+    for ic_type, ic_values in internal_coordinates.items():
+
+        if len(ic_values) == 0:
+            continue
+
+        qm_values, mm_values = zip(*ic_values)
+
+        qm_values = numpy.array(qm_values)
+        mm_values = numpy.array(mm_values)
+
+        # Converting from radians to degrees
+        if ic_type in ["Angle", "Dihedral", "Improper"]:
+            rmsd = compute_rmsd(qm_values*180/numpy.pi, mm_values*180/numpy.pi, 360)
+        else:
+            rmsd = compute_rmsd(qm_values, mm_values)
+
+        internal_coordinate_rmsd[ic_type] = float(rmsd)
+
+    fb_objective = 0.0
+    for key, value in internal_coordinate_rmsd.items():
+        if key == "Bond":
+            fb_objective += (1/0.1)*internal_coordinate_rmsd["Bond"]
+        elif key == "Angle":
+            fb_objective += (1/8.0)*internal_coordinate_rmsd["Angle"]
+        elif key == "Dihedral":
+            fb_objective += (1/20.0) * internal_coordinate_rmsd["Dihedral"]
+        elif key == "Improper":
+            fb_objective += (1/20.0) * internal_coordinate_rmsd["Improper"]
+
+    return internal_coordinate_rmsd, fb_objective
+
+
+def _compute_metrics(
+    label: str, input_dictionary: Dict[str, Any], index: int
+) -> pandas.DataFrame:
+
+    record_id_tag = input_dictionary["record-id-tag"]
+
+    qm_tag = input_dictionary["qm-tag"]
+    mm_tag = input_dictionary["mm-tag"]
+
+    qm_molecule_stream = oechem.oemolistream(
+        input_dictionary["qm-structure"] + f"-{index}.sdf"
+    )
+    qm_molecule_stream.SetConfTest(oechem.OEAbsoluteConfTest(False))
+
+    mm_molecule_stream = oechem.oemolistream(
+        input_dictionary["mm-structures"][label] + f"-{index}.sdf"
+    )
+    mm_molecule_stream.SetConfTest(oechem.OEAbsoluteConfTest(False))
+
+    metrics = []
+
+    for oe_qm_molecule, oe_mm_molecule in zip(
+        qm_molecule_stream.GetOEMols(), mm_molecule_stream.GetOEMols()
+    ):
+
+        conformer_metrics = []
+
+        for conformer_index, (oe_qm_conformer, oe_mm_conformer) in enumerate(
+            zip(oe_qm_molecule.GetConfs(), oe_mm_molecule.GetConfs())
+        ):
+
+            smiles = oechem.OEGetSDData(oe_qm_conformer, "SMILES QCArchive")
+            record_id = oechem.OEGetSDData(oe_qm_conformer, record_id_tag)
+
+            assert (
+                oechem.OEGetSDData(oe_mm_conformer, record_id_tag) == record_id
+            ), f"conformer mismatch {oe_qm_conformer.GetTitle()}"
+
+            qm_energy = float(oechem.OEGetSDData(oe_qm_conformer, qm_tag))
+            mm_energy = float(oechem.OEGetSDData(oe_mm_conformer, mm_tag))
+
+            rmsd = oechem.OERMSD(
+                oe_qm_conformer,
+                oe_mm_conformer,
+                RMSD_AUTOMORPH,
+                RMSD_HEAVY_ONLY,
+                RMSD_OVERLAY,
+            )
+
+            qm_molecule = Molecule.from_openeye(
+                oechem.OEMol(oe_qm_conformer), allow_undefined_stereo=True
+            )
+            mm_molecule = Molecule.from_openeye(
+                oechem.OEMol(oe_mm_conformer), allow_undefined_stereo=True
+            )
+
+            internal_coordinate_rmsds, fb_objective = _compute_internal_coordinate_rmsd(
+                qm_molecule, qm_molecule.conformers[0], mm_molecule.conformers[0]
+            )
+
+            rd_qm_molecule = qm_molecule.to_rdkit()
+            rd_mm_molecule = mm_molecule.to_rdkit()
+
+            try:
+                tfd = TorsionFingerprints.GetTFDBetweenMolecules(
+                    rd_qm_molecule, rd_mm_molecule
+                )
+
+            except IndexError:
+
+                print(
+                    f"error calculating TFD for id={record_id} - possibly no "
+                    f"non-terminal rotatable bonds found."
+                )
+
+                tfd = np.nan
+
+            conformer_metrics.append(
+                {
+                    "SMILES": smiles,
+                    "Conformer Idx": conformer_index,
+                    "QM Energy": qm_energy,
+                    "MM Energy": mm_energy,
+                    "RMSD": rmsd,
+                    **{
+                        f"{ic_type} RMSD": ic_rmsd
+                        for ic_type, ic_rmsd in internal_coordinate_rmsds.items()
+                    },
+                    "TFD": tfd,
+                    "Record ID": record_id,
+                }
+            )
+
+        # Compute the dde metric.
+        qm_energies = numpy.array([metric["QM Energy"] for metric in conformer_metrics])
+        mm_energies = numpy.array([metric["MM Energy"] for metric in conformer_metrics])
+
+        lowest_qm_energy_idx = qm_energies.argmin()
+
+        relative_qm_energies = qm_energies - qm_energies[lowest_qm_energy_idx]
+        relative_mm_energies = mm_energies - mm_energies[lowest_qm_energy_idx]
+
+        relative_qm_energies[lowest_qm_energy_idx] = numpy.nan
+        relative_mm_energies[lowest_qm_energy_idx] = numpy.nan
+
+        dde = relative_mm_energies - relative_qm_energies
+
+        metrics.extend(
+            {
+                "Force Field": label,
+                **{
+                    key: value
+                    for key, value in conformer_metric.items()
+                    if "Energy" not in key
+                },
+                "ddE": conformer_dde,
+            }
+            for conformer_dde, conformer_metric in zip(dde, conformer_metrics)
+        )
+
+    return pandas.DataFrame(metrics)
+
+
+@click.command()
+@click.option(
+    "--input",
+    "input_path",
+    type=click.Path(exists=True, dir_okay=False),
+    default="03-force-fields.json",
+)
+@click.option(
+    "--index",
+    "index",
+    type=click.INT,
+)
+@click.option(
+    "--output",
+    "output_path",
+    type=click.Path(exists=False, dir_okay=False),
+)
+def main(input_path, index, output_path):
+
+    with open(input_path) as file:
+        input_dictionary = json.load(file)
+
+    method_labels = [*input_dictionary["mm-structures"]]
+
+    print(method_labels)
+    metrics = pandas.concat(
+        [_compute_metrics(label, input_dictionary, index) for label in method_labels]
+    )
+    metrics.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    main()
